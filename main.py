f3d45c14f73ee1dbfeb35583bbc886da96455746
Matias Mutuan <matiastm99@gmail.com>
2023-08-13 23:33:27 -0300

    Update file
    Did i say it was finished? Haha, not really. I removed the queries and the menu and put them in new files. This way the main program is cleaner. I think...

--- main.py
+++ main.py
@@ -1,33 +1,6 @@
 import sqlite3
-
-def initDB(cur):
-    cur.execute("CREATE TABLE IF NOT EXISTS books(title varchar(255), status varchar(255));")
-
-def addBook(cur, title, status):
-    cur.execute("INSERT INTO books VALUES (?,?);", (title, status))
-
-def updateBook(cur, title, status):
-    cur.execute("UPDATE books SET status=? WHERE title=?;", (status, title))
-
-def deleteBook(cur, title):
-    cur.execute("DELETE FROM books WHERE title=?;", (title, ))
-
-def showAllByName(cur):
-    for row in cur.execute("SELECT * FROM books ORDER BY title ASC;"):
-        print(f"Title: {row[0].title()} - Status: {row[1].title()}")
-
-def showAllByStatus(cur):
-    for row in cur.execute("SELECT * FROM books ORDER BY status ASC"):
-        print(f"Title: {row[0].title()} - Status: {row[1].title()}")
-
-def printMenu():
-    print("")
-    print("1. Add a book")
-    print("2. Update a book status")
-    print("3. Delete a book")
-    print("4. Show all books")
-    print("0. Exit")
-    print("")
+from menuPrinter import printMenu
+from sqlQueries import initDB, add, update, delete, showAllByName, showAllByStatus
 
 if __name__ == "__main__":
     con = sqlite3.connect("books.db")
@@ -41,14 +14,14 @@ if __name__ == "__main__":
         if opt == 1:
             title = input("Enter the title: ").lower()
             status = input("Enter the status: ").lower()
-            addBook(cur, title, status)
+            add(cur, title, status)
         elif opt == 2:
             title = input("Enter the title: ").lower()
             status = input("Enter the status: ").lower()
-            updateBook(cur, title, status)
+            update(cur, title, status)
         elif opt == 3:
             title = input("Enter the title: ").lower()
-            deleteBook(cur, title)
+            delete(cur, title)
         elif opt == 4:
             print("")
             show = int(input("Show by name(1) or status(2): "))
