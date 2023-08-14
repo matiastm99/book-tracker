e1f90a2b8942563c791f894d71a105068dbe3636
Matias Mutuan <matiastm99@gmail.com>
2023-08-13 23:28:08 -0300

    Add new file
    Add menu printer

---
+++ menuPrinter.py
@@ -0,0 +1,8 @@
+def printMenu():
+    print("")
+    print("1. Add a book")
+    print("2. Update a book status")
+    print("3. Delete a book")
+    print("4. Show all books")
+    print("0. Exit")
+    print("")
